8ef98f767842a7bbd610e99b35e19a6a6b28d13a
Ninetails <jrgome03@louisville.edu>
2018-04-06 17:17:43 -0400

    New Folder (UniFees)
    new json file

---
+++ UniversityFees/fees.js
@@ -0,0 +1,75 @@
+[
+	{
+		"feeType": "Foreign Student Services Fee",
+		"feeSubject": {
+			"Fall": {
+				"feeAmount":"$50"
+			},
+			"Spring": {
+				"feeAmount":"$50"
+			},
+			"Summer": {
+				"feeAmount":"$25"
+			}
+		}
+	},
+	{
+		"feeType": "School Fees",
+		"feeSubject": {
+			"Student Recreation Fee": {
+				"feeAmount": "$98 (Less than full-time will be pro-rated per credit hour based on academic level)"
+			},
+			"Traditional Residence Hall Meal Plan": {
+				"feeAmount": "$1,597"
+			},
+			"Apartment Residence Hall Meal Plan": {
+				"feeAmount": "$1,020"
+			}
+		}
+	},
+	{
+		"feeType": "Course Fees",
+		"feeSubject": {
+			"American Sign Language": {
+				"class": ["ASL 205", "ASL 305"],
+				"feeAmount": "$50 per course" 
+			},
+			"Audiology Clinical Practicum Fee": {
+				"class": ["AUDI 610", "AUDI 620", "AUDI 635"],
+				"feeAmount": "$125 per course" 
+			},
+			"Audiology Lab Fee": {
+				"class": ["AUDI 614", "AUDI 615", "AUDI 622", "AUDI 624", "AUDI 630", "AUDI 654"],
+				"feeAmount": "$25 per course" 
+			}
+		}
+	},
+	{
+		"feeType": "Health Fees",
+		"feeSubject": {
+			"Health Professional Health Fee": {
+				"feeAmount":"$52.50"
+			},
+			"Major Medical Health Insurance": {
+				"feeAmount":"$1,172.50"
+			},
+			"Major Medical & PCH Fee": {
+				"feeAmount":"$1,275"
+			}
+		}
+	},
+	{
+		"feeType": "Other Fees",
+		"feeSubject": {
+			"Late Payment Fee": {
+				"feeAmount":"$100"
+			},
+			"Reinstatement Fee": {
+				"feeAmount":"$50"
+			},
+			"Returned Check Fee": {
+				"feeAmount":"$25"
+			}
+		}
+	}
+]
