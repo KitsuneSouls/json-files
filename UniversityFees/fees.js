9f65663d9160f3e236830a341f809865705e9c59
Ninetails <jrgome03@louisville.edu>
2018-04-11 15:18:57 -0400

    Updated
    All fees added

--- UniversityFees/fees.js
+++ UniversityFees/fees.js
@@ -24,6 +24,45 @@
 			},
 			"Apartment Residence Hall Meal Plan": {
 				"feeAmount": "$1,020"
+			},
+			"UG Belknap Commuter Meal Plan": {
+				"feeAmount": "$250"
+			},
+			"Education and Human Development Teacher Education Fee (TEAD)": {
+				"feeAmount": "$62.50"
+			},
+			"Dental Enhancement Fee": {
+				"feeAmount": "Varies by Specialty"
+			},
+			"Dental Instrument Fee": {
+				"feeAmount": "$3,500"
+			},
+			"Graduate Business Fee (Up to 9 Credit Hours)": {
+				"feeAmount": "$10 per Credit Hour"
+			},
+			"Graduate UPA Fee (Up to 9 Credit Hours)": {
+				"feeAmount": "$12.50 per Credit Hour"
+			},
+			"A&S Technology Fee": {
+				"feeAmount": "$.50 per Credit Hour"
+			},
+			"Law Technology Fee": {
+				"feeAmount": "$50"
+			},
+			"Medical Technology Fee": {
+				"feeAmount": "$350"
+			},
+			"SOM Medical Disability Insurance Fee - (FALL ONLY)": {
+				"feeAmount": "$46"
+			},
+			"Music Majors Fee": {
+				"feeAmount": "$175 pro-rated"
+			},
+			"Speed Software/Technology Fee": {
+				"feeAmount": "$25"
+			},
+			"Public Health Technology Fee": {
+				"feeAmount": "$100"
 			}
 		}
 	},
@@ -44,6 +83,143 @@
 			},
 			"Biology Lab Fee": {
 				"feeAmount": "$15 - $40*"
+			},
+			"Business Technology/Career Management Fee": {
+				"feeAmount": "$45 per Course*"
+			},
+			"Business Zero Credit Internship": {
+				"class": ["ACCT 396", "ECON 396", "EQIN 396", "FIN 396", "CIS 396", "MKT 396"],
+				"feeAmount": "$45 per course" 
+			},
+			"Chemistry Lab Fee": {
+				"feeAmount": "$10 - $60 per Course*"
+			},
+			"CIS Technology Fee": {
+				"class": ["CIS 100"],
+				"feeAmount": "$30 per course" 
+			},
+			"Classic and Modern Languages Lab Fee": {
+				"feeAmount": "$15 - $25 per Course*" 
+			},
+			"Sports/Activities Fee": {
+				"feeAmount": "$50 per Course*"
+			},
+			"Health & Sports Sciences Fee": {
+				"class": ["HSS 396", "EXP 601"],
+				"feeAmount": "$95 per Course*"
+			},
+			"Health & Sports Sciences Lab Fee": {
+				"class": ["HSS 387", "HSS 388", "HSS 395"],
+				"feeAmount": "$40 per Course*"
+			},
+			"Kent School Practicum Course Fee": {
+				"class": ["SW 470", "SW 471", "SW 670", "SW 671", "SW 672", "SW 673"],
+				"feeAmount": "$25 per Course*"
+			},
+			"Math Computer Lab Fee": {
+				"feeAmount": "$60 per Course*"
+			},
+			"Music-Applied Music Fee for Non-Music Majors": {
+				"feeAmount": "$400 per Course*"
+			},
+			"Music-Secondary Applied Music Fee for Non-Music Majors": {
+				"feeAmount": "$200 per Course*"
+			},
+			"Music Accessory Fee": {
+				"class": ["MUS 137", "MUS 229", "MUS 235", "MUS 252", "MUS 253", "MUS 254", "MUS 335", "MUS 336", "MUED 556"],
+				"feeAmount": "$10 per Course*"
+			},
+			"Music Woodwinds Accessory Fee": {
+				"class": ["MUS 236"],
+				"feeAmount": "$25 per Course"
+			},
+			"Nursing Instructional Course Fee": {
+				"class-1": ["N 356"],
+				"feeAmount-1": "$210 per Course",
+				"class-2": ["N 372"],
+				"feeAmount-2": "$180 per Course",
+				"class-3": ["N 346", "N 403", "N 408", "N 464", "N 471", "N 474"],
+				"feeAmount-3": "$150 per Course",
+				"class-4": ["N 338"],
+				"feeAmount-4": "$140 per Course",
+				"class-5": ["N 344", "N 345", "N 357", "N 407", "N 461"],
+				"feeAmount-5": "$120 per Course",
+				"class-6": ["N 395"],
+				"feeAmount-6": "$110 per Course",
+				"class-7": ["N 347", "N 402", "N 404", "N 405", "N 406", "N 463"],
+				"feeAmount-7": "$90 per Course",
+				"class-8": ["N 358", "N 466", "N 472", "N 473"],
+				"feeAmount-8": "$60 per Course",
+				"class-9": ["N 401"],
+				"feeAmount-9": "$30 per Course"
+			},
+			"Nursing DNP Clinical Course Fee": {
+				"class": ["N 751", "N 789"],
+				"feeAmount": "$300"
+			},
+			"Nursing Clinical Course Fee": {
+				"class-1": ["N 474"],
+				"feeAmount-1": "$225",
+				"class-2": ["N 356", "N 372"],
+				"feeAmount-2": "$135",
+				"class-3": ["N 346", "N 402", "N 408", "N 409", "N 464", "N 473"],
+				"feeAmount-3": "$90",
+				"class-4": ["N 344", "N 345", "N 357","N 403", "N 405", "N 406", "N 407", "N 461"],
+				"feeAmount-4": "$45",
+				"class-5": ["N 404"],
+				"feeAmount-5": "$22.50"
+			},
+			"Nursing Graduate Specialty Fee": {
+				"class-1": ["N 615", "N 622", "N 624", "N 633", "N 645", "N 661", "N 665", "N 672"],
+				"feeAmount-1": "$200 per Course*",
+				"class-2": ["N 629", "N 656"],
+				"feeAmount-2": "$100 per Course*"
+			},
+			"Nursing Acute Care NP Clinical Fee": {
+				"class": ["N 675"],
+				"feeAmount": "$200 per Course*"
+			},
+			"Nursing DNP Instructional Fee": {
+				"class": ["N 740", "N 746", "N 748", "N 751", "N 755", "N 756", "N 757","N 763", "N 764", "N 770", "N 771", "N 772", "N 779", "N 780", "N 784", "N 789"],
+				"feeAmount": "$50 per Course*"
+			},
+			"Political Science Lab Fee": {
+				"class": ["POL 520", "POL 530"],
+				"feeAmount": "$60 per Course*"
+			},
+			"Studio Art Fee	": {
+				"feeAmount": "$25 - $75 per Course*"
+			},
+			"Theater Arts Fee": {
+				"feeAmount": "$20 per Course*"
+			},
+			"Astronomy Lab Fee": {
+				"feeAmount": "$25 per Course*"
+			},
+			"Atmospheric Science Course Fee": {
+				"class": ["PHYS 350", "PHYS 360", "PHYS 365", "PHYS 465", "PHYS 466", "PHYS 469"],
+				"feeAmount": "$50 per Course*"
+			},
+			"Speed School Course Fee": {
+				"class": ["BE 605"],
+				"feeAmount": "$100 per Course*"
+			},
+			"Speed Engineering Coop Course Fee": {
+				"class": ["SSoE 289", "SSoE 389", "SSoE 489"],
+				"feeAmount": "$300 per Course*"
+			},
+			"Speed (ECE) Lab Fee": {
+				"class": ["ECE 544"],
+				"feeAmount": "$100 per Course*"
+			},
+			"Speed Engineering Biomedical Microfluidic Lab Fee": {
+				"feeAmount": "$125 per Course*"
+			},
+			"International Teaching Experience (College of Education)": {
+				"feeAmount": "Varies by Course - Contact the Academic Department"
+			},
+			"Study Abroad Programs": {
+				"feeAmount": "Varies by Course - Contact the Academic Department"
 			}
 		}
 	},
@@ -58,6 +234,9 @@
 			},
 			"Major Medical & PCH Fee": {
 				"feeAmount":"$1,275"
+			},
+			"Primary Care Health Fee": {
+				"feeAmount": "$150"
 			}
 		}
 	},
@@ -72,6 +251,15 @@
 			},
 			"Returned Check Fee": {
 				"feeAmount":"$25"
+			},
+			"Extramural Exam Credit": {
+				"feeAmount": "1/2 Regular Course Fee"
+			},
+			"Replacement Diploma": {
+				"feeAmount": "$20"
+			},
+			"Transcript Fee": {
+				"feeAmount": "$7"
 			}
 		}
 	}
